9eef4ad03c2a1abd70d477d5c25ab536163eec78
LianYuguai <1652727694@qq.com>
2023-02-12 17:00:19 +0800

    feat: 修改mock数据

--- mock/chart/index.js
+++ mock/chart/index.js
@@ -6,7 +6,12 @@ export default [
       return {
         code: '0',
         success: true,
-        data: []
+        data: [{
+          toolChamber: '11111/2 3 4 5',
+          tool: '2222',
+          cham: '3333',
+          matchRatio: 99.1
+        }]
       }
     }
   },
